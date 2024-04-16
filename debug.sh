aa56648cba0c00be7d8100138b6b3abdd4c89c5e
Rob Chiocchio <rmchiocchio@wpi.edu>
2024-04-16 17:09:03 -0400

    Kill script

--- debug.sh
+++ debug.sh
@@ -1,3 +1,9 @@
+#/bin/bash
 flatpak run --command=calibre-debug com.calibre_ebook.calibre -s
+
+flatpak ps | grep com.calibre_ebook.calibre | grep -oP "(^\\S+)" | while read -r pid; do
+    flatpak kill $pid
+done
+
 flatpak run --command=calibre-customize com.calibre_ebook.calibre -b "calibre-plugin"
 flatpak run --command=calibre-debug com.calibre_ebook.calibre -g
