d4d85ca312d3476dc7cd7440c9493ce961a1c86e
Rob Chiocchio <rmchiocchio@wpi.edu>
2024-04-16 15:01:51 -0400

    Docker debug script

---
+++ debug.sh
@@ -0,0 +1,3 @@
+flatpak run --command=calibre-debug com.calibre_ebook.calibre -s
+flatpak run --command=calibre-customize com.calibre_ebook.calibre -b "calibre-plugin"
+flatpak run --command=calibre-debug com.calibre_ebook.calibre -g
