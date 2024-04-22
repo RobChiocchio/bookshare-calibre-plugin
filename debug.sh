a4ef22ea181d1133ba03e4d1cf9349ec8d234094
Rob Chiocchio <rmchiocchio@wpi.edu>
2024-04-21 22:41:00 -0400

    Debug script accepts args

--- debug.sh
+++ debug.sh
@@ -6,4 +6,4 @@ flatpak ps | grep com.calibre_ebook.calibre | grep -oP "(^\\S+)" | while read -r
 done
 
 flatpak run --command=calibre-customize com.calibre_ebook.calibre -b "calibre-plugin"
-flatpak run --command=calibre-debug com.calibre_ebook.calibre -g
+flatpak run --command=calibre-debug com.calibre_ebook.calibre -g $@
