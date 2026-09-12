f44cda5e22100f0e0f8139e1e188fb525a0b6021
Rob Chiocchio <1686473+RobChiocchio@users.noreply.github.com>
2026-09-12 16:19:27 +0200

    Support native and Flatpak for build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,26 @@
-#/bin/bash
-flatpak run --command=calibre-debug com.calibre_ebook.calibre -e ./build_ui.py $(flatpak document-export -trw calibre-plugin/ -a com.calibre_ebook.calibre)
+#!/bin/bash
+set -euo pipefail
+
+CMD="calibre-debug"
+FLATPAK_ID="com.calibre_ebook.calibre"
+SCRIPT="./build_ui.py"
+SRC_DIR="calibre-plugin/"
+FLAGS="-e"
+
+# Check if native Calibre binary exists
+if command -v "$CMD" >/dev/null 2>&1; then
+    echo "Building with Calibre Native"
+    "$CMD" -e "$SCRIPT" -- "$SRC_DIR" "$@" # TODO: test this
+elif flatpak info "$FLATPAK_ID" >/dev/null 2>&1; then
+    echo "Building with Calibre Flatpak"
+
+    # Export source directory to the sandbox
+    FLATPAK_SRC_DIR=$(flatpak document-export -trw "$SRC_DIR" -a "$FLATPAK_ID")
+
+    # Cleanup exports on exit
+    trap 'flatpak document-unexport "$FLATPAK_SRC_DIR" >/dev/null 2>&1' EXIT
+
+    flatpak run --command="$CMD" "$FLATPAK_ID" "$FLAGS" "$SCRIPT" -- "$FLATPAK_SRC_DIR" "$@"
+else
+    echo "No Calibre version detected"
+fi
--- debug.sh
+++ debug.sh
@@ -1,9 +1,34 @@
-#/bin/bash
-flatpak run --command=calibre-debug com.calibre_ebook.calibre -s
+#!/bin/bash
+set -euo pipefail
 
-flatpak ps | grep com.calibre_ebook.calibre | grep -oP "(^\\S+)" | while read -r pid; do
-    flatpak kill $pid
-done
+FLATPAK_ID="com.calibre_ebook.calibre"
+SRC_DIR="calibre-plugin/"
+DEBUG_CMD="calibre-debug"
+CUSTOMIZE_CMD="calibre-customize"
+IS_FLATPAK=false
 
-flatpak run --command=calibre-customize com.calibre_ebook.calibre -b "calibre-plugin"
-flatpak run --command=calibre-debug com.calibre_ebook.calibre -g $@
+if flatpak info "$FLATPAK_ID" >/dev/null 2>&1; then
+    echo "Using Flatpak"
+    IS_FLATPAK=false
+    DEBUG_CMD=(flatpak run --command="$DEBUG_CMD" "$FLATPAK_ID")
+    CUSTOMIZE_CMD=(flatpak run --command="$CUSTOMIZE_CMD" "$FLATPAK_ID")
+fi
+
+# Request shutdown running instances of Calibre
+"${DEBUG_CMD[@]}" "-s"
+
+sleep 1
+
+# Force kill any remaining processes
+if $IS_FLATPAK; then
+    flatpak ps | grep $FLATPAK_ID | grep -oP "(^\\S+)" | while read -r pid; do
+        flatpak kill $pid
+    done
+fi
+
+# Kill remaining native process instances
+pkill -f calibre
+
+# Load plugin
+"${CUSTOMIZE_CMD[@]}" "-b" "$SRC_DIR"
+"${DEBUG_CMD[@]}" "-g" "$@"
