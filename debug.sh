c294d2e9543f187f20da9e98c0dfd803e4728ff2
Rob Chiocchio <1686473+RobChiocchio@users.noreply.github.com>
2026-09-12 19:53:50 +0200

    Fix setting flag

--- debug.sh
+++ debug.sh
@@ -5,13 +5,14 @@ FLATPAK_ID="com.calibre_ebook.calibre"
 SRC_DIR="calibre-plugin/"
 DEBUG_CMD="calibre-debug"
 CUSTOMIZE_CMD="calibre-customize"
-IS_FLATPAK=false
 
 if flatpak info "$FLATPAK_ID" >/dev/null 2>&1; then
     echo "Using Flatpak"
-    IS_FLATPAK=false
+    is_flatpak=true
     DEBUG_CMD=(flatpak run --command="$DEBUG_CMD" "$FLATPAK_ID")
     CUSTOMIZE_CMD=(flatpak run --command="$CUSTOMIZE_CMD" "$FLATPAK_ID")
+else
+    is_flatpak=false
 fi
 
 # Request shutdown running instances of Calibre
@@ -20,7 +21,7 @@ fi
 sleep 1
 
 # Force kill any remaining processes
-if $IS_FLATPAK; then
+if $is_flatpak; then
     flatpak ps | grep $FLATPAK_ID | grep -oP "(^\\S+)" | while read -r pid; do
         flatpak kill $pid
     done
